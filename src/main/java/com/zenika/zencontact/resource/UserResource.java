1a56a21a0bd80a121a888128ce2bf70000eb93e6
zebraska <cyril.contant@ars.sante.fr>
2019-02-11 14:20:23 +0100

    correct package

--- src/main/java/com/zenika/zencontact/resource/UserResource.java
+++ src/main/java/com/zenika/zencontact/resource/UserResource.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 // With @WebServlet annotation the webapp/WEB-INF/web.xml is no longer required.
 @WebServlet(name = "UserResource", value = "/api/v0/users")
