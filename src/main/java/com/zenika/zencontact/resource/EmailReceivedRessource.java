bdcc1c21c1f881748158afa3f59b2a51493686ea
zebraska <cyril.contant@ars.sante.fr>
2019-02-13 13:50:36 +0100

    add email service

---
+++ src/main/java/com/zenika/zencontact/resource/EmailReceivedRessource.java
@@ -0,0 +1,12 @@
+package com.zenika.zencontact.ressource;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@WebServlet(name="EmailReceivedRessource", value="_ah/mail/*")
+@ServletSecurity(@HttpConstraint(rolesAllowed={"admin"}))
+public class EmailReceivedRessource extends HttpServlet{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+
+    }
+}
